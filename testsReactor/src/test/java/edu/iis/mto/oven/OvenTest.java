1fd6d1556f5dc77be44d7919361cebd28fe2b31d
gorazdnina <ninagorazd@gmail.com>
2021-05-14 16:11:58 +0200

    Added shouldInvokeHeatingModuleHeaterWithExpectedSettingsWhenInitializing

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,18 +1,50 @@
 package edu.iis.mto.oven;
 
+import static edu.iis.mto.oven.Oven.HEAT_UP_AND_FINISH_SETTING_TIME;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    @Mock
+    private HeatingModule heatingModule;
+
+    @Mock
+    private Fan fan;
+
+    Oven oven;
+    ProgramStage sampleProgramStage = ProgramStage.builder().withStageTime(10).withHeat(HeatType.GRILL).withTargetTemp(150).build();
+    BakingProgram sampleBakingProgram = BakingProgram.builder().withInitialTemp(50).withStages(List.of(sampleProgramStage)).build();
+
+    @BeforeEach
+    public void setUp(){
+        oven = new Oven(heatingModule, fan);
+    }
+
     @Test
-    void itCompiles() {
-        MatcherAssert.assertThat(true, equalTo(true));
+    void shouldInvokeHeatingModuleHeaterWithExpectedSettingsWhenInitializing() {
+        BakingProgram sampleBakingProgram = BakingProgram.builder().withInitialTemp(50).withStages(Collections.emptyList()).build();
+        HeatingSettings expectedSettings = HeatingSettings.builder()
+                .withTargetTemp(sampleBakingProgram.getInitialTemp())
+                .withTimeInMinutes(HEAT_UP_AND_FINISH_SETTING_TIME)
+                .build();
+        doNothing().when(heatingModule).heater(expectedSettings);
+
+        oven.start(sampleBakingProgram);
+
+        verify(heatingModule).heater(expectedSettings);
     }
 
 }
